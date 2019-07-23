0323e6d1e565493b1c47001480ab0c25d040e547
dmitriy.belgiets <chegidim@gmail.com>
2019-07-23 22:34:23 +0300

    added saving updated date to firebase

--- lib/FBDB/FBDB.cpp
+++ lib/FBDB/FBDB.cpp
@@ -29,13 +29,10 @@ int FBDB::getFeed(void) {
 void FBDB::disableFeed(void) {
   DynamicJsonBuffer jsonBuffer;
 
-  JsonObject &feedObject = jsonBuffer.createObject();
-  //JsonObject &timestamp = feedObject.createNestedObject("updated");
+  JsonObject& feedObject = jsonBuffer.createObject();
   feedObject["status"] = 0;
-  feedObject["updated"] = "timestamp";
+  JsonObject& updatedObject = feedObject.createNestedObject("updated");
+  updatedObject[".sv"] = "timestamp";
 
-  Firebase.push(_path, feedObject);
-
-  // Firebase.setInt(_statusPath, 0);
-  // Firebase.setString(_updatedPath, serverTimestamp);
+  Firebase.set(_path, feedObject);
 };
